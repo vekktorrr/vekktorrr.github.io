eaf928486d0710ae079556f006aaecd432a47bdf
vekktorrr <90112055+vekktorrr@users.noreply.github.com>
2021-11-21 18:07:50 +0300

    Add files via upload
    jquery

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,33 +1,53 @@
-"use strict";
+$(document).ready(function(){
+
+    let options = {threshold: [0.5]};
+    let observer = new IntersectionObsever(onEntry, options);
+    let elements = $('.element-animation');
+    elements.each((i, el) =>{
+    observer.observe(el);
+   });
+    
+    
+    function onEntry (entry){
+        entry.forEach(change => {
+            if(change.isIntersecting){
+                change.target.classList.add('show-animation');
+                
+            }
+        });
+    }
+   
+    
+  $('.image-link').magnificPopup({type:'image'});
+});
+    
+   /* $(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+    
+    $(".section").each(i, el) => {
+        if ($(el)).offset().top - $("nav").outerHeight() <= scrollDistance{
+            $("nav a").each((i, el)) => {
+                if ($(el).hasClass("active")){
+                    $(el).removeClass("active");
+                }
+            }); 
+            $('nav li:eq('+ i +')').find('a).addClass('active);
+        }
+   
+    
+    }
+});
+}); */
+    
+    
+/*
+$('a[href^="#"]').click(function(){
+    let valHref = $(this).attr("href");
+    $('html, body').animate({scrollTop: $(valHref).offset().top + "px"});
+});
+*/
+    
 
 
-var nameUser=prompt("Здравствуйте, как вас зовут?");
 
-    alert("Рад вас видеть, "+ nameUser);
-
-
-prompt("Какой тип сайта Вы хотели бы?");
-
-
-prompt("Какой дизайн Вы хотите?");
-
-
-
-prompt("Укажите тип вёрстки?");
-alert("Благодарю за Ваш выбор! Сейчас калькулятор всё рассчитает :) ");
-
-
-
-let calculator = {
-    typeSite: 1750,
-    design: 1500,
-    verstka: 3200,
-};
-
-let sum = 0;
-for( let key in calculator){
-    sum += calculator[key];
-}
-
-alert (sum); // 6450
-
+   
